80dd87570c18f537a9f66ccd4e435056e6e47ad5
win-cmd <68722061+win-cmd@users.noreply.github.com>
2023-05-21 21:34:26 +0800

    Create population.c

---
+++ population.c
@@ -0,0 +1,37 @@
+//The lab work of the week 1.
+//population.c
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int start = 0;
+    int end = 0;
+    do
+    {
+        start = get_int("Strat size: ");
+    }
+    while (start < 9);
+
+    // TODO: Prompt for end size
+    do
+    {
+        end = get_int("End size: ");
+    }
+    while (end < start);
+
+    // TODO: Calculate number of years until we reach threshold
+    int n = start;
+    int year = 0;
+    while (n < end)
+    {
+        int birth = n / 3;
+        int death = n / 4;
+        n = n + birth - death;
+        year++;
+    }
+
+    // TODO: Print number of years
+    printf("Years: %i\n", year);
+}
