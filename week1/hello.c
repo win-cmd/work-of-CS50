455b6b297d1f97005e44cabac87df1fd681b7631
win-cmd <68722061+win-cmd@users.noreply.github.com>
2023-05-23 16:36:11 +0800

    Create hello.c

---
+++ week1/hello.c
@@ -0,0 +1,11 @@
+//This is a work of week1
+//hello.c
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string name = get_string("what's your name?");
+    printf("hello, %s\n", name);
+}
