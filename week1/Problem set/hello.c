f436d07fce8c04da1a2c78a7af1e83d5662259fc
win-cmd <68722061+win-cmd@users.noreply.github.com>
2023-05-26 11:30:01 +0800

    Update hello.c

--- week1/Problem set/hello.c
+++ week1/Problem set/hello.c
@@ -1,1 +1,11 @@
+//This is a problem of week1
+//hello.c
 
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string name = get_string("what's your name?");
+    printf("hello, %s\n", name);
+}
